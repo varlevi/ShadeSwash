41b9ef280cc44bc9a23e7d400231526898284483
varlevi <53223556+varlevi@users.noreply.github.com>
2020-02-25 20:37:32 -0600

    Update styles.js for settings pane prototype
    adds button functionality

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ document.getElementsByClassName('submit')[0].addEventListener('click', (event) =
 
   print('<h2>Here are your results:</h2>');
   print( '<h3>Base Color: <br></h3>' );
-  print( `<section class="card"></br> <p>rgb(${red}, ${green}, ${blue})</p> </br> ${html} </br></section>` );
+  print( `<section class="card">${html}</br><p>rgb(${red}, ${green}, ${blue})</p></section>` );
   for (i = 0; i < 5; i += 1) {
     print( ' </br>' );
   }
@@ -69,6 +69,7 @@ document.getElementsByClassName('submit')[0].addEventListener('click', (event) =
   }
 
   //Prints all Shades
+
   print( '<h3>Shades: <br></h3><section class="colors-div">' );
 
   while (red < 256 && green < 256 && blue < 256) {
@@ -76,7 +77,7 @@ document.getElementsByClassName('submit')[0].addEventListener('click', (event) =
     rgbColor = baseColor();
     html = `<div style="background-color: ${rgbColor}" class="darkmode-ignore colors"></div>`
 
-    print( `<section class="card"></br> <p>rgb(${red}, ${green}, ${blue})</p> </br> ${html} </br></section>` );
+    print( `<section class="card">${html}</br><p>rgb(${red}, ${green}, ${blue})</p></section>` );
 
     red += 2;
     blue += 2;
@@ -86,3 +87,59 @@ document.getElementsByClassName('submit')[0].addEventListener('click', (event) =
   print('</section>');
 
 });
+
+//Custom settings panel
+
+let menuButton = document.getElementById("menu-button");
+let menuState = false;
+menuButton.addEventListener('click', () => {
+  menuState = !menuState;
+  if (menuState == true) {
+    menuButton.style.transform = "scale(1.2)";
+    document.getElementById('github-button').style.display = "block";
+    document.getElementById('dark-button').style.display = "block";
+    document.getElementById('github-button').style.opacity = "1.0";
+    document.getElementById('dark-button').style.opacity = "1.0";
+  } else {
+    menuButton.style.transform = "scale(1)";
+    document.getElementById('github-button').style.display = "none";
+    document.getElementById('dark-button').style.display = "none";
+    document.getElementById('github-button').style.opacity = "0.0";
+    document.getElementById('dark-button').style.opacity = "0.0";
+  }
+})
+
+let darkButton = document.getElementById('dark-button');
+darkModeState = false;
+darkButton.addEventListener('click', () => {
+  darkModeState = !darkModeState;
+  let h3 = document.getElementsByTagName('H3');
+  let buttons = document.getElementsByClassName('button')
+  if (darkModeState == true) {
+    document.body.style.backgroundColor = "#111";
+    document.getElementsByTagName('h1')[0].style.color = '#DDD';
+    document.getElementsByTagName('h2')[0].style.color = '#DDD';
+    for (i of buttons) {
+      i.style.color = '#333';
+    }
+    for (i of h3) {
+      i.style.color = '#DDD';
+    }
+  } else {
+    document.body.style.backgroundColor = "#FFF";
+    document.getElementsByTagName('h1')[0].style.color = '#384047';
+    document.getElementsByTagName('h2')[0].style.color = '#384047';
+    for (i of buttons) {
+      i.style.color = '#222';
+    }
+    for (i of h3) {
+      i.style.color = '#333';
+    }
+  }
+})
+
+let githubButton = document.getElementById('github-button');
+githubModeState = false;
+githubButton.addEventListener('click', () => {
+  window.location.href = 'https://github.com/varlevi/the_shade_generator';
+})
